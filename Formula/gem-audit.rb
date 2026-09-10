80b805e8f49c7634d70cc1e6ccf2a9eabc8f3bb5
Zac <579103+7a6163@users.noreply.github.com>
2026-09-10 11:10:05 +0800

    Bump gem-audit to 2.11.0 and update SHA-256 checksums

--- Formula/gem-audit.rb
+++ Formula/gem-audit.rb
@@ -1,28 +1,28 @@
 class GemAudit < Formula
   desc "Ultra-fast, standalone security auditor for Gemfile.lock, written in Rust"
   homepage "https://github.com/7a6163/gem-audit"
-  version "2.10.0"
+  version "2.11.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/7a6163/gem-audit/releases/download/v#{version}/gem-audit-aarch64-apple-darwin.tar.gz"
-      sha256 "9800754db66160ae4a997d963f761f2f59127ecdf324dd923c45c27a74c096d3"
+      sha256 "08f907ced58c36854cd59502e22c651995db513c5fd3d81bcee3dee4b6b68584"
     end
     on_intel do
       url "https://github.com/7a6163/gem-audit/releases/download/v#{version}/gem-audit-x86_64-apple-darwin.tar.gz"
-      sha256 "4842837a124fabcbdef2d7c6f62e03e509757221caea5187add33920473de599"
+      sha256 "09f83223c00bd06002fe3244c261dad7715efd2bce1a56c66bb0698394fdfc4d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/7a6163/gem-audit/releases/download/v#{version}/gem-audit-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9cca707114e6215d5a45e626be616fe986221d4e8f29a7c954065f94b83726ca"
+      sha256 "5aa2d534e33afca0b45da3cbcb92a478c6055c30f3fd56d537e7d522a012cc6e"
     end
     on_intel do
       url "https://github.com/7a6163/gem-audit/releases/download/v#{version}/gem-audit-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "727e552d081aea5bbc0a3246b4152d887b4ecc5558fff3c2a548137ac59e569e"
+      sha256 "8b80e1a6d88fc0f1521842ecc464b814cc2a7f00fcb0c8fd202fad9a20521960"
     end
   end
 
